7f48b0c044257b23ab8d2d755a9d98c1f1739e4c
Helvijs <helvijs.adams@gmail.com>
2017-05-10 09:59:12 +0200

    update

---
+++ Software/EMS/EMS/main.c
@@ -0,0 +1,18 @@
+/*
+ * EMS.c
+ *
+ * Created: 10.05.2017 09:58:57
+ * Author : Helvijs Adams
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
