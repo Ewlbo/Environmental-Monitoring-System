376b6c0353940c21336d1849207a5c5a872fcf7c
Helvijs <helvijs.adams@gmail.com>
2017-05-16 17:38:56 +0200

    Revert "Update"
    This reverts commit 9feddc8699bcf4e3bc3c4af35d447dde246d70a4.

--- Software/EMS/EMS/Debug/makedep.mk
+++ Software/EMS/EMS/Debug/makedep.mk
@@ -4,5 +4,3 @@
 
 main.c
 
-RF\nrf24l01.c
-
--- Software/EMS/EMS/main.c
+++ Software/EMS/EMS/main.c
@@ -1,6 +1,11 @@
-
+/*
+ * EMS.c
+ *
+ * Created: 10.05.2017 09:58:57
+ * Author : Helvijs Adams
+ */ 
+// Søren did this
 #include <avr/io.h>
-#include <util/delay.h>
 
 
 int main(void)
